597ba6e8892607d58583919d6928f73dcd691022
chlbri <bri_lvi@icloud.com>
2022-04-14 20:28:04 +0000

    style: add button

--- src/components/buttons/Basket.tsx
+++ src/components/buttons/Basket.tsx
@@ -5,7 +5,9 @@ export const BtnBasket: FC = () => {
   return (
     <div className="flex space-x-2 items-center">
       <Icon_ShopBasket />
-      <div className="font-semibold text-poussin text-sm">Panier</div>
+      <button className="font-semibold text-poussin text-sm">
+        Panier
+      </button>
     </div>
   );
 };
