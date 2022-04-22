e9959f98cefd56017f0dc7cfe33b3f2c17c6963a
chlbri <bri_lvi@icloud.com>
2022-04-22 00:06:16 +0000

    style: Refine

--- src/machines/main/machine.ts
+++ src/machines/main/machine.ts
@@ -6,89 +6,87 @@ import { MainEvents } from './events';
 
 const voidAsync = () => Promise.resolve() as never;
 
-export const mainMachine =
-  /** @xstate-layout N4IgpgJg5mDOIC5QFsCGBLAdgOnRANmAMQDKAKgIIBKZioADgPazoAu6jmdIAHogLQBmAOwAGbCMEAmAGwBGOQBZBATilLhAGhABPAYtEzsolcMEyArFOUnFigL73taLNgDGACzBuA1ligAopgAbugATpzIYJisAGqoYeioAEaEsEQQnGC4IYw+2QhYwYxuqOycCDKoAO6GAK5hsMLcTCzlXEi8+sJy2MLWABxSUhZy5gYD2noI-HIW4hYyohaCcvIy6nKOzhg49GFg9An+GVk5xfnYhbml7Qiwecic9MiiLcxsHB2gfDOqKtgBpZlNZTMtFFpdIg5ooJBYVHZRFJBCjFHNtiAXDhYKwEuxMFBTphskU8gVSbcvggoKgwKIIDxUDJqu82l9uL8hAMBtg0cMVANFBZFAMTCipog7BZsCpBKNlGoVAjDI4nCBMIwIHBuFjcAQwKzPpwOQJVIJjGMBrLRAMxMI0ZDpvwVsIZaIzBZhDJhPNPSoMbrPN4-ASgqEIpgojF4okUmlDe0TTNFCpelIVKMRAMzYoRhK-lIeYYVkpkfakf61br9odjgSE+zOpy0QDlJ6-UphQM5PnnTyhkC1kiNnIzJWdq4cXj-A3jU39O6ZUMluZuT1ZfnZObvYpLMJU4Mxg4q7tsFOwqxILPvl0-mpsFuBvMESJRhn81ZYUNBECVKIbUClgBrs15Js6cwPhmYzCNmsq5hYvbyOIf5mDISw9LmtqqvYQA */
-  createMachine(
-    {
-      initial: 'idle',
-      tsTypes: {} as import('./machine.typegen').Typegen0,
-      context,
-      schema: {
-        context: {} as MainContext,
-        events: {} as MainEvents,
+export const mainMachine = createMachine(
+  {
+    initial: 'idle',
+    tsTypes: {} as import('./machine.typegen').Typegen0,
+    context,
+    schema: {
+      context: {} as MainContext,
+      events: {} as MainEvents,
+    },
+    states: {
+      idle: {
+        exit: 'inc',
+        on: {
+          START: {
+            target: 'checkingEnvironmentVariables',
+          },
+        },
       },
-      states: {
-        idle: {
-          exit: 'inc',
-          on: {
-            START: {
-              target: 'checkingEnvironmentVariables',
+      checkingEnvironmentVariables: {
+        exit: 'inc',
+        invoke: {
+          src: 'checkEnvironmentsVariables',
+          onDone: [
+            {
+              target: 'preparing',
             },
-          },
+          ],
         },
-        checkingEnvironmentVariables: {
-          exit: 'inc',
-          invoke: {
-            src: 'checkEnvironmentsVariables',
-            onDone: [
-              {
-                target: 'preparing',
-              },
-            ],
-          },
-        },
-        preparing: {
-          exit: 'inc',
-          invoke: {
-            src: 'prepare',
-            onDone: [
-              {
-                target: 'starting',
-              },
-            ],
-          },
-        },
-        starting: {
-          exit: 'inc',
-          invoke: {
-            src: 'start',
-            onDone: {
-              target: '#started',
+      },
+      preparing: {
+        exit: 'inc',
+        invoke: {
+          src: 'prepare',
+          onDone: [
+            {
+              target: 'starting',
             },
+          ],
+        },
+      },
+      starting: {
+        exit: 'inc',
+        invoke: {
+          src: 'start',
+          onDone: {
+            target: '#started',
           },
         },
-        started: {
-          type: 'parallel',
-          id: 'started',
+      },
+      started: {
+        type: 'parallel',
+        id: 'started',
 
-          states: {
-            locales: {
-              entry: 'startLocalesMachine',
-              exit: 'inc',
-              id: 'locales',
-              invoke: {
-                id: 'localesMachine',
-                src: 'localesMachine',
+        states: {
+          locales: {
+            entry: 'startLocalesMachine',
+            exit: 'inc',
+            id: 'locales',
+            invoke: {
+              id: 'localesMachine',
+              src: 'localesMachine',
+            },
+            tags: 'important',
+            on: {
+              'LOCALES.UPDATE': {
+                actions: 'updateLocale',
+                target: '.busy',
               },
-              tags: 'important',
-              on: {
-                'LOCALES.UPDATE': {
-                  actions: 'updateLocale',
-                  target: '.busy',
+            },
+            initial: 'busy',
+            states: {
+              busy: {
+                after: {
+                  170: 'notBusy',
                 },
               },
-              states: {
-                busy: {
-                  after: {
-                    170: 'notBusy',
-                  },
-                },
-                notBusy: {
-                  on: {
-                    'LOCALES.CHANGE_LANGUAGE': {
-                      actions: 'changeLocale',
-                      target: 'busy',
-                    },
+              notBusy: {
+                on: {
+                  'LOCALES.CHANGE_LANGUAGE': {
+                    actions: 'changeLocale',
+                    target: 'busy',
                   },
                 },
               },
@@ -96,31 +94,32 @@ export const mainMachine =
           },
         },
       },
-      id: 'main',
     },
-    {
-      actions: {
-        inc: assign(context => {
-          context.iterator++;
+    id: 'main',
+  },
+  {
+    actions: {
+      inc: assign(context => {
+        context.iterator++;
+      }),
+      changeLocale: send(
+        (_, { locale }) => ({
+          type: 'CHANGE_LANGUAGE',
+          locale,
         }),
-        changeLocale: send(
-          (_, { locale }) => ({
-            type: 'CHANGE_LANGUAGE',
-            locale,
-          }),
-          { to: 'localesMachine' },
-        ),
-        startLocalesMachine: send('START', { to: 'localesMachine' }),
-        updateLocale: assign((context, { current, locale }) => {
-          context.locales.current = current;
-          context.locales.locale = locale;
-        }),
-      },
-      services: {
-        localesMachine,
-        checkEnvironmentsVariables: voidAsync,
-        prepare: voidAsync,
-        start: voidAsync,
-      },
+        { to: 'localesMachine' },
+      ),
+      startLocalesMachine: send('START', { to: 'localesMachine' }),
+      updateLocale: assign((context, { current, locale }) => {
+        context.locales.current = current;
+        context.locales.locale = locale;
+      }),
     },
-  );
+    services: {
+      localesMachine,
+      checkEnvironmentsVariables: voidAsync,
+      prepare: voidAsync,
+      start: voidAsync,
+    },
+  },
+);
